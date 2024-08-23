173d8a4d02da7a746f7380e566ec231d01926ea8
Randall Hand <randall.hand@gmail.com>
2024-08-23 14:42:22 -0400

    Safety - if log is empty, don't process

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ bluetoothctl disconnect DA:AB:E8:9C:B4:31
 date
 ~/.local/bin/meshtastic --ble DA:AB:E8:9C:B4:31 --info >meshtastic.log
 date
-
-python3 script.py
-cp meshtastic_nodes.html /var/www/html/reh.html
+if [ -s meshtastic.log ]; then
+  python3 script.py
+  cp meshtastic_nodes.html /var/www/html/reh.html
+fi
