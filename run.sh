e2179e518cbc6da145eb7f4c98066eab227a8a72
Randall Hand <randall.hand@gmail.com>
2024-08-23 11:03:01 -0400

    Added a disconnect

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-meshtastic --ble DA:AB:E8:9C:B4:31 --info >meshtastic.log
+date
+cd /home/yeraze/Development/pymeshdisplay
+bluetoothctl disconnect DA:AB:E8:9C:B4:31
+~/.local/bin/meshtastic --ble DA:AB:E8:9C:B4:31 --info >meshtastic.log
 
 python3 script.py
 cp meshtastic_nodes.html /var/www/html/reh.html
