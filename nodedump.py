66becdefb88a4434a237b4b0e19a55c2322eee27
Randall Hand <randall.hand@gmail.com>
2024-10-10 06:38:41 -0400

    Rename of script -> makemap

--- nodedump.py
+++ nodedump.py
@@ -1,8 +1,25 @@
 import meshtastic
 import meshtastic.ble_interface
+from pubsub import pub
+import time
 
 iface = meshtastic.ble_interface.BLEInterface("DA:AB:E8:9C:B4:31")
-if iface.nodes:
-    __import__("pprint").pprint(iface.nodes)
+
+
+def onReceive(packet, interface):
+    if "decoded" in packet:
+        message_bytes = packet["decoded"]["payload"]
+        message_string = message_bytes.decode("utf-8")
+        fptr = open("messages.txt", "a")
+        fptr.write(message_string)
+        fptr.write("\n")
+        fptr.close()
+
+
+pub.subscribe(onReceive, "meshtastic.receive.text")
+
+for i in range(0, 10):
+    print("Waiting for response.. %i" % i)
+    time.sleep(1)
 
 iface.close()
